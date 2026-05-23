b5e34d7f7a4e932414be1dbd5d31dff719d51618
aikipezman-pixel <aikipezman@gmail.com>
2026-05-23 13:19:41 -0400

    WestTexasCommit

--- script.js
+++ script.js
@@ -5,3 +5,40 @@ function openMenu() {
 function closeMenu() {
     document.body.classList.remove("menu--open");
 }
+const lightboxLinks = document.querySelectorAll(".artwork-lightbox-link");
+const lightbox = document.querySelector("#lightbox");
+const lightboxImg = document.querySelector("#lightboxImg");
+const lightboxClose = document.querySelector("#lightboxClose");
+
+lightboxLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const image = link.querySelector("img");
+
+    lightboxImg.src = link.href;
+    lightboxImg.alt = image.alt;
+
+    lightbox.classList.add("active");
+  });
+});
+
+function closeLightbox() {
+  lightbox.classList.remove("active");
+  lightboxImg.src = "";
+  lightboxImg.alt = "";
+}
+
+lightboxClose.addEventListener("click", closeLightbox);
+
+lightbox.addEventListener("click", (event) => {
+  if (event.target === lightbox) {
+    closeLightbox();
+  }
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    closeLightbox();
+  }
+});
